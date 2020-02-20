6d1455ffa2599f95869d22c27804f3e6132f15f3
Aidesiderio Alhambra <deri.alhambra@gmail.com>
2020-02-19 18:25:21 -0800

    event handler for catch from activity.html added

---
+++ public/assets/js/index.js
@@ -0,0 +1,24 @@
+$(function(){
+  //event handler for catch history to db
+  $("#fishLocationSubmitButton").on("submit", function(event){
+    event.preventDefault();
+    console.log("New catch testing");
+
+    var newCatch = {
+      fish_type: $("#fishType").val().trim(),
+      bait_type: $("baitType").val().trim(),
+      location: newMarker.location
+    };
+
+    //Send the POST for newCatch to db 
+    $.ajax("/api/catch", {
+      type: "POST",
+      data: newCatch
+    }).then({
+      function() {
+        console.log("Created New Catch!");
+        location.reload();
+      }
+    })
+  })
+})
