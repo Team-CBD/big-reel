57103aa9064bcc555a83c939fbfd94702ef1ce92
Aidesiderio Alhambra <deri.alhambra@gmail.com>
2020-02-21 09:24:46 -0800

    seed.sql added

---
+++ seed.sql
@@ -0,0 +1,17 @@
+USE reelbig_db;
+
+INSERT INTO catchhistory (fish_type, lat, lng, userId) 
+VALUES ("bass", 33.866136, -117.181405, 1); 
+
+INSERT INTO catchhistory (fish_type, lat, lng, userId) 
+VALUES ("trout", 33.868832, -117.160453, 1);
+
+INSERT INTO catchhistory (fish_type, lat, lng, userId) 
+VALUES ("bass", 33.864458, -117.166086, 2);
+
+INSERT INTO catchhistory (fish_type, lat, lng, userId) 
+VALUES ("bass", 33.863995, -117.173971, 3);
+
+INSERT INTO profile (username) VALUES ("Sally951");
+INSERT INTO profile (username) VALUES ("Bob123");
+INSERT INTO profile (username) VALUES ("Rick808");
